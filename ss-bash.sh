d3fa52580a8b40e82e952a405b8333da0643a716
atxiu <33857802+atxiu@users.noreply.github.com>
2018-01-02 12:39:37 +0800

    Add files via upload

--- ss-bash.sh
+++ ss-bash.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 start()
 {
-    v=$[63210+$(ls /etc/shadowsocks-libev/ -l |grep "^-"|wc -l)]
+    port=$[63210+$(ls /etc/shadowsocks-libev/ -l |grep "^-"|wc -l)]
     pw=$(cat /dev/urandom | head -1 | md5sum | head -c 10)
     server=$(ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:")
-    echo $server
 (
 cat <<EOF
 {
 "server":"$server",
-"server_port":$v,
+"server_port":$port,
 "password":"$pw",
 "timeout":300,
 "method":"chacha20-ietf-poly1305"
@@ -19,8 +18,15 @@ EOF
     systemctl start shadowsocks-libev-server@$1.service
     systemctl enable shadowsocks-libev-server@$1.service
     cat /etc/shadowsocks-libev/$1.json
-    ss=ss://$(python _base64.py -en chacha20-ietf-poly1305:$pw@ss.atxiu.cn:$v)
+    ss=ss://$(python _base64.py -en chacha20-ietf-poly1305:$pw@$server:$port)
     echo $ss
+    time=$(date -d '+1 month' "+%M %H %d %m")
+(
+cat <<EOF
+0 0 0 0 0 echo "crontab ok"
+EOF
+)>/var/spool/cron/root
+    sed -i "1i $time * /bin/bash ~/ss-bash.sh stop $1" /var/spool/cron/root 
 }
 stop()
 {
