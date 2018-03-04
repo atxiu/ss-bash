dc914bea3009a4c059807b9a8f009d7464aa5cd3
atxiu <33857802+atxiu@users.noreply.github.com>
2018-03-04 15:24:25 +0800

    添加 net-tools

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-yum install -y epel-release yum-plugin-copr wget
+yum install -y epel-release yum-plugin-copr wget net-tools
 yum copr -y enable librehat/shadowsocks
 yum install -y shadowsocks-libev
 
